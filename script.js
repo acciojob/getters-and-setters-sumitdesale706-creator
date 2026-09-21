44a6b5fed6b75b8d688459dafcf5d99e99c1ca6a
Sumit Desale <sumitdesale706@gmail.com>
2026-09-22 02:10:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name, age){
+		 this.name = name;
+		 this.age = age;
+	}
 
-class Student extends Person {}
+	getName(){
+		 return this.name;
+	}
 
-class Teacher extends Person {}
+	setAge(age){
+		 this.age = age;
+	}
+}
+
+class Student extends Person {
+	study(){
+		console.log(`${this.name} is studying`);
+	}
+}
+
+class Teacher extends Person {
+	teach(){
+		console.log(`${this.name} is teaching`)
+	}
+}
+
+
 
 // Do not change the code below this line
 window.Person = Person;
